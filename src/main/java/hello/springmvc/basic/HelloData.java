d5a6b08c88196082526efaddb25236d38b8e2f4f
aishatrue <140340401+aishatrue@users.noreply.github.com>
2024-04-19 06:09:07 +0900

    model attribute

--- src/main/java/hello/springmvc/basic/HelloData.java
+++ src/main/java/hello/springmvc/basic/HelloData.java
@@ -1,4 +1,12 @@
 package hello.springmvc.basic;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+//getter,setter를 자동으로 만들어줌.
 public class HelloData {
+    private String username;
+    private int age;
 }
